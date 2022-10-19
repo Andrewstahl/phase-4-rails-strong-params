ba15c111f11c9ee7c84a070a758142fd2ba57904
Andrew Stahl <andrewstahl96@gmail.com>
2022-10-19 19:43:46 -0400

    All files working

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,4 +1,6 @@
 class BirdsController < ApplicationController
+  # The reason for this is that Rails by default will wrap JSON parametersLinks to an external site. as a nested hash under a key based on the name of the controller (in our case, bird since we're in a BirdsController). This is the reason that in the Rails server log, even with our strong params in place, you'll still see Unpermitted parameters: :bird for our requests.
+  wrap_parameters format: []
 
   # GET /birds
   def index
@@ -8,7 +10,7 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(name: params[:name], species: params[:species])
+    bird = Bird.create(bird_params)
     render json: bird, status: :created
   end
 
@@ -20,6 +22,13 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+  end
+
+  private
+
+  # When we call params.permit(:name, :species), this will return a new hash with only the name and species keys. Rails will also mark this new hash as permitted, which means we can safely use this new hash for mass assignment
+  def bird_params
+    params.permit(:name, :species)
   end
 
 end
